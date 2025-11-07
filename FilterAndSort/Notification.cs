1eec67d2682153605c841352a6da7c306ae186ee
faybit <andrvata01@gmail.com>
2025-11-07 13:23:34 +0300

    Added Library

---
+++ FilterAndSort/Notification.cs
@@ -0,0 +1,10 @@
+﻿public class Notification
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string? Content { get; set; }
+    public NotificationType Type { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Priority { get; set; } // 0 (низкий) до 5 (высокий)
+    public bool IsRead { get; set; }
+}
---
+++ FilterAndSort/SortNotificationBy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum SortNotificationBy
+{
+    Date,
+    Priority,
+    Title
+}
